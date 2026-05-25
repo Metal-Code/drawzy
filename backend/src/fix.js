e4a68f02644b9875c1b1924763f1277a5a44c998
Ayush Kumar <metalbond69@gmail.com>
2026-05-25 11:37:52 +0530

    before CI/CD

--- backend/src/fix.js
+++
@@ -1,5 +0,0 @@
-import Database from 'better-sqlite3'
-const db = new Database('./drawzy.db')
-db.prepare("UPDATE users SET avatar = '🎨' WHERE username = 'ayush'").run()
-console.log('Fixed!')
-db.close()
